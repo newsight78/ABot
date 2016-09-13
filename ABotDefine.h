943a684a217a3d5cb71338fc0524f9eba397a9ec
kihoon <newsight78@gmail.com>
2016-09-13 15:02:37 +0900

    Define header 파일 추가.

---
+++ ABotDefine.h
@@ -0,0 +1,17 @@
+
+#ifndef _ABOTDEFINE_
+#define _ABOTDEFINE_
+
+enum eWinVersion
+{
+	eNotSupportedVersion,
+	eWindowsXP,
+	eWindows_Vista,
+	eWindows_7,
+	eWindows_8,
+	eWindows_8_1,
+	eWindows_10,
+	eWinVerMax,
+};
+
+#endif
